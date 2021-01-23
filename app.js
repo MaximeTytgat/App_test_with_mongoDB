4a0ba3e685defa96959dd8ab55819a95ff5d797b
TYTGAT Maxime <maxime2103i@gmail.com>
2021-01-23 11:53:59 +0100

    reset project

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const express = require('express')
+const app = express()
+const port = 9999
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
+app.get('/', (req, res) => {
+    res.send('Hezzzllo World!')
+})
 
-// Database Name
-const dbName = 'myproject';
-
-// Create a new MongoClient
-const client = new MongoClient(url);
-
-// Use connect method to connect to the Server
-client.connect(function(err) {
-    assert.equal(null, err);
-    console.log("Connected successfully to server");
-
-    const db = client.db(dbName);
-
-    client.close();
-});
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
